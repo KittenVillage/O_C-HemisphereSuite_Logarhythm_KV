895de98fc0c91ae231c26b83d48014dabd8d67b3
KittenVillage <kittenvillage@gmail.com>
2021-03-01 22:19:58 -0500

    Update HEM_Sequence5.ino
    Changed Seq5 to Seq8 (uncommenting Logarhythm's code)

--- software/o_c_REV/HEM_Sequence5.ino
+++ software/o_c_REV/HEM_Sequence5.ino
@@ -20,15 +20,21 @@
 
 #include "HSMIDI.h"
 
+
+// APD Modified: Clocks are locked out when reset is still high
+#define SEQ5_STEPS 8
+// Now 8 (test)
+// NOTE: Not claiming >5 when saving, or restoring when loading!
+
 class Sequence5 : public HemisphereApplet {
 public:
 
     const char* applet_name() { // Maximum 10 characters
-        return "Sequence5";
+        return "Sequence8";
     }
 
     void Start() {
-        for (int s = 0; s < 5; s++) note[s] = random(0, 30);
+        for (int s = 0; s < SEQ5_STEPS; s++) note[s] = random(0, 30);
         play = 1;
     }
 
@@ -47,8 +53,10 @@ public:
         play_note = constrain(play_note, 0, 127);
 
         if (Clock(0)) StartADCLag();
-
-        if (EndOfADCLag()) {
+        
+        
+        if (EndOfADCLag() && !Gate(1))  // Logarhythm: Don't clock if currently within a reset pulse, so overlapping clock+reset pulses go to step 0 instead of 1 and reset can "hold"
+        {
             Advance(step);
             if (step == 0) ClockOut(1);
             play = 1;
@@ -66,7 +74,7 @@ public:
     }
 
     void OnButtonPress() {
-        if (++cursor == 5) cursor = 0;
+        if (++cursor == SEQ5_STEPS) cursor = 0;
     }
 
     void OnEncoderMove(int direction) {
@@ -111,32 +119,52 @@ protected:
 private:
     int cursor = 0;
     char muted = 0; // Bitfield for muted steps; ((muted >> step) & 1) means muted
-    int note[5]; // Sequence value (0 - 30)
+    int note[SEQ5_STEPS]; // Sequence value (0 - 30)
     int step = 0; // Current sequencer step
     bool play; // Play the note
 
     void Advance(int starting_point) {
-        if (++step == 5) step = 0;
+        if (++step == SEQ5_STEPS) step = 0;
         // If all the steps have been muted, stay where we were
         if (step_is_muted(step) && step != starting_point) Advance(starting_point);
     }
 
     void DrawPanel() {
         // Sliders
-        for (int s = 0; s < 5; s++)
+        for (int s = 0; s < SEQ5_STEPS; s++)
         {
-            int x = 6 + (12 * s);
+            //int x = 6 + (12 * s);
+            int x = 6 + (7 * s); // APD:  narrower to fit more
+            
             if (!step_is_muted(s)) {
                 gfxLine(x, 25, x, 63);
 
                 // When cursor, there's a heavier bar and a solid slider
                 if (s == cursor) {
                     gfxLine(x + 1, 25, x + 1, 63);
-                    gfxRect(x - 4, BottomAlign(note[s]), 9, 3);
-                } else gfxFrame(x - 4, BottomAlign(note[s]), 9, 3);
-
+                    //gfxRect(x - 4, BottomAlign(note[s]), 9, 3);
+                    gfxRect(x - 2, BottomAlign(note[s]), 5, 3);  // APD
+                } else 
+                {
+                  //gfxFrame(x - 4, BottomAlign(note[s]), 9, 3);
+                  gfxFrame(x - 2, BottomAlign(note[s]), 5, 3);  // APD
+                }
+                
                 // When on this step, there's an indicator circle
-                if (s == step) {gfxCircle(x, 20, 3);}
+                if (s == step) 
+                {
+
+                  
+                  gfxCircle(x, 20, 3);  //Original
+
+                  // APD
+                  //int play_note = note[step];// + 60 + transpose;
+
+                  //gfxPrint(10, 15, "Scale ");
+                  //gfxPrint(cursor < 12 ? 1 : 2);
+                  //gfxPrint(x, 20, play_note);
+                  
+                }
             } else if (s == cursor) {
                 gfxLine(x, 25, x, 63);
                 gfxLine(x + 1, 25, x + 1, 63);
